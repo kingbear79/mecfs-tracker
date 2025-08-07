75630ac50b090cfb08978ea1ada5abf9b193c252
kingbear79 <73781651+kingbear79@users.noreply.github.com>
2025-08-07 08:42:31 +0200

    feat: register blocks for editor and bump version

--- blocks/chart/index.js
+++ blocks/chart/index.js
@@ -1,1 +1,17 @@
-(()=>{"use strict";window.wp.i18n,window.wp.blockEditor,window.ReactJSXRuntime})();
+( function( blocks, element, i18n ) {
+    const { registerBlockType } = blocks;
+    const { createElement } = element;
+    const { __ } = i18n;
+
+    registerBlockType( 'mecfs/chart', {
+        title: __( 'MECFS Tracker Diagramm', 'mecfs-tracker' ),
+        icon: 'chart-bar',
+        category: 'widgets',
+        edit: function() {
+            return createElement( 'p', {}, __( 'MECFS Tracker Diagramm', 'mecfs-tracker' ) );
+        },
+        save: function() {
+            return null;
+        }
+    } );
+} )( window.wp.blocks, window.wp.element, window.wp.i18n );
--- blocks/form/index.js
+++ blocks/form/index.js
@@ -1,1 +1,17 @@
-(()=>{"use strict";window.wp.i18n,window.wp.blockEditor,window.ReactJSXRuntime})();
+( function( blocks, element, i18n ) {
+    const { registerBlockType } = blocks;
+    const { createElement } = element;
+    const { __ } = i18n;
+
+    registerBlockType( 'mecfs/form', {
+        title: __( 'MECFS Tracker Formular', 'mecfs-tracker' ),
+        icon: 'clipboard',
+        category: 'widgets',
+        edit: function() {
+            return createElement( 'p', {}, __( 'MECFS Tracker Formular', 'mecfs-tracker' ) );
+        },
+        save: function() {
+            return null;
+        }
+    } );
+} )( window.wp.blocks, window.wp.element, window.wp.i18n );
